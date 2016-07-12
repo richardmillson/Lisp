dd29ead978e950d534676264d91fbbebc2f6622d
richardmillson <8217613+richardmillson@users.noreply.github.com>
2016-07-12 17:23:15 -0400

    Find node and neighbours of node in graph

--- bfs.lisp
+++ bfs.lisp
@@ -10,3 +10,13 @@
 (defun set-graph (graph-as-list)
   (setf graph graph-as-list))
 
+(defun find-node (node-name)
+  (defun next-node (unchecked-graph)
+    (cond ((null unchecked-graph) nil)
+          ((equal node-name (caar unchecked-graph)) (car unchecked-graph))
+          (t (next-node (cdr unchecked-graph)))))
+  (next-node graph))
+
+(defun neighbours (node-name)
+  (cdr (find-node node-name)))
+
