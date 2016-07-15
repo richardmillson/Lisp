033ffb86f145f6d71656e9e443c9b8e2621aa051
richardmillson <8217613+richardmillson@users.noreply.github.com>
2016-07-15 10:51:58 -0400

    Add queue

--- bfs.lisp
+++ bfs.lisp
@@ -29,3 +29,29 @@
   ; (member desired (find-node given)))
   (appears-in desired (find-node given)))
 
+; queue implementation
+
+(defun init-queue ()
+  (setq queue nil))
+
+(defun enqueue (element)
+  (setf queue (append queue (list element)))
+  element)
+
+(defun enqueue-list (llist)
+  (unless (null llist)
+    (enqueue (car llist))
+    (enqueue-list (cdr llist)))
+  t)
+
+(defun front ()
+  (car queue))
+
+(defun list-queue ()
+  queue)
+
+(defun dequeue ()
+  (let ((front-element (front)))
+    (setf queue (cdr queue))
+    front-element))
+
