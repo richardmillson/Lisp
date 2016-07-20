c2b0acf1eb91e76550f979832b59b3c042dd4239
richardmillson <8217613+richardmillson@users.noreply.github.com>
2016-07-20 14:37:48 -0400

    Add enqueue list function

--- queue.lisp
+++ queue.lisp
@@ -1,9 +1,17 @@
+; queue implementation
+
 (defun init-queue ()
   (setq queue nil))
 
 (defun enqueue (element)
   (setf queue (append queue (list element)))
   element)
+
+(defun enqueue-list (llist)
+  (unless (null llist)
+    (enqueue (car llist))
+    (enqueue-list (cdr llist)))
+  t)
 
 (defun front ()
   (car queue))
