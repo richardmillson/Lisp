881012dd33685752b2ba5d882ba4f8a676b996c2
richardmillson <8217613+richardmillson@users.noreply.github.com>
2016-07-20 14:40:00 -0400

    Add push list to stack

--- stack.lisp
+++ stack.lisp
@@ -1,9 +1,17 @@
+; stack implementation
+
 (defun init-stack ()
   (setq stack nil))
 
 (defun push (element)
   (setf stack (append (list element) stack))
   element)
+
+(defun push-list (llist)
+  (unless (null llist)
+    (push (car llist))
+    (push-list (cdr llist)))
+  t)
 
 (defun top ()
   (car stack))
