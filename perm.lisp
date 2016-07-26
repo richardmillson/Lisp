d6635387e2e9dc433de8531718d262fdcd4a7a51
richardmillson <8217613+richardmillson@users.noreply.github.com>
2016-07-26 14:19:11 -0400

    Implement the permutation group

---
+++ perm.lisp
@@ -0,0 +1,15 @@
+; implement permutations
+
+; return the sign of a permutation
+; sgn(alpha) = (-1)^{n-r}
+; alpha is a permutation
+; n is the cardinality of the symmetric group that alpha belongs to ie S_n
+; r is the number of disjoint cycles in alpha's complete factorization
+(defun sgn (alpha)
+  nil)
+
+; (defun evenp (alpha)
+;   (= 1 (sgn alpha)))
+
+; (defun oddp (alpha)
+;   (= -1 (sgn alpha)))
