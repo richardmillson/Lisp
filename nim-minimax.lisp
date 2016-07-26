3bfe11d204c9bb0c4078a5cdd1f204b081a2f1f6
richardmillson <8217613+richardmillson@users.noreply.github.com>
2016-07-26 13:44:29 -0400

    Detect losing state

--- nim-minimax.lisp
+++ nim-minimax.lisp
@@ -1,5 +1,13 @@
 ; assume that min starts. 
-; thus an odd number of piles mean it is min's turn
+; hence an odd number of piles mean it is min's turn
 
 (defun play (piles)
+  (when (lostp piles) ; is losing state
+    (if (oddp (length piles)) ; is min's move
+        1
+        0)) ; is max's move
   )
+
+; a losing state is a pile containing one or more 2's and some number of 1's
+(defun lostp (piles)
+  (and (or (= 1 car(piles)) (= 2 (car piles))) (lostp (cdr piles))))
