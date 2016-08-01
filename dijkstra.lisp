1f23e63a4f639fbd5da71263762e31559e335e2a
richardmillson <8217613+richardmillson@users.noreply.github.com>
2016-08-01 14:19:31 -0400

    Fix cost-to-neighbour to work with names

--- dijkstra.lisp
+++ dijkstra.lisp
@@ -1,32 +1,43 @@
-; Dijkstra's algorithm for finding the shortest path between nodes in a weighted graph
-
+;;; dijkstra's algorithm 
+;;; find the shortest path between nodes in a weighted graph
 (defun dijkstra (graph start)
   (init-path-costs graph)
   (update-path-cost start 0)
   (init-predecessors graph)
   (init-visited)
   (init-candidates start)
+  ;; current is a name
   (loop while candidates do
         (let ((current (min-candidate)))
           (add-visited current)
-          (loop for neighbour in (set-difference (neighbours current) visited :test #'equal) do
-                (cond (< (+ (path-cost (name current)) 
-                            (cost-to-neighbour neighbour))
-                         (path-cost neighbour)) 
-                      ((update-path-cost (name neighbour)
-                                         (+ (path-cost (name current)) 
-                                            (cost-to-neighbour neighbour)))
-                       (update-predecessor (name neighbour)
-                                           (name current)))
-                      ((not (member (name neighbour) visited))
-                       (add-visited (name neighbour)))))
-          finally predecessors)))
+          ; fix visited is only names, neighbours is node type
+          ; rewrite cost-to
+          (loop for neighbour in (set-difference (list-nodes (neighbours current)) 
+                                                 visited 
+                                                 :test #'equal)
+                do 
+                (cond 
+                  ((< (+ (path-cost current)
+                         (cost-to-neighbour neighbour))
+                      (path-cost neighbour))
+                   (update-path-cost neighbour
+                                     (+ (path-cost current) 
+                                        (cost-to-neighbour neighbour)))
+                   (update-predecessor neighbour current))
+                  ((not (member neighbour visited))
+                   (add-visited neighbour)))
+                finally predecessors))))
 
-; keep track of the smallest cost to get to each node found by dijkstra
-; note that all paths start at the start node called in dijkstra
+(defun only-names (list-nodes))
+
+;;; keep track of the smallest cost to get to each node found by dijkstra
+;;; note that all paths start at the start node called in dijkstra
 (defun init-path-costs (graph)
-  (let ((impossible (+ 1 (apply '+ (remove-if-not #'integerp (flatten graph))))))
-    (setq path-path-costs (mapcar #'(lambda (node) (list node impossible)) (list-nodes graph)))))
+  (let ((impossible (+ 1 (apply '+ (remove-if-not #'integerp 
+                                                  (flatten graph))))))
+    (setq path-costs 
+          (mapcar #'(lambda (node) (list node impossible)) 
+                  (list-nodes graph)))))
 
 (defun flatten (llist)
   (cond ((null llist) nil)
@@ -34,19 +45,26 @@
         (t (append (flatten (car llist)) (flatten (cdr llist))))))
 
 (defun update-path-cost (end path-cost)
-  (setq path-costs (append (remove-if #'(lambda (node) (equal (car node) end)) path-costs) (list (list end path-cost)))))
+  (setq path-costs 
+        (append (remove-if #'(lambda (node) (equal (car node) end)) 
+                           path-costs) 
+                (list (list end path-cost)))))
 
-; takes the name of a node
+;;; takes the name of a node
 (defun path-cost (target)
-  (cadar (remove-if-not #'(lambda (node) (equal (car node) target)) path-costs)))
+  (cadar (remove-if-not #'(lambda (node) (equal (car node) target)) 
+                        path-costs)))
 
-; keep track of the predecessor of each node on the shortest path to the start node
+;;; keep track of the predecessor of each node on the shortest path to the start node
 (defun init-predecessors (graph)
-  (setq predecessors (mapcar #'(lambda (node) (list node nil)) (list-nodes graph))))
+  (setq predecessors (mapcar #'(lambda (node) (list node nil)) 
+                             (list-nodes graph))))
 
-; end and predecessor are names of a nodes
+;;; end and predecessor are names of a nodes
 (defun update-predecessor (end predecessor)
-  (setq predecessors (append (remove-if #'(lambda (node) (equal (car node) end)) predecessors) (list (list end predecessor)))))
+  (setq predecessors (append (remove-if #'(lambda (node) (equal (car node) end))
+                                        predecessors) 
+                             (list (list end predecessor)))))
 
 (defun init-visited ()
   (setq visited nil))
@@ -58,13 +76,15 @@
   (setq candidates (list start)))
 
 (defun add-candidate (node)
-  (setq candidate (append candidate (list node)))  
-  (setq candidates (sort candidates (lambda (left right)  (< (path-cost left) (path-cost right))))))
+  (setq candidates (append candidates (list node)))  
+  (setq candidates (sort candidates 
+                         (lambda (left right) (< (path-cost left) 
+                                                 (path-cost right))))))
 
 (defun min-candidate ()
   (pop candidates))
 
-; nodes are stored as (name cost)
+;;; nodes are stored as (name cost)
 (defun cost-to-neighbour (node)
   (cadr node))
 
@@ -72,12 +92,12 @@
   (car node))
 
 
-; weighted directed graph implementation
-; the graph is a list of nodes
-; a node is a list whose first element is the name of the node
-; and whose remaining elements are pairs of a neighbours name 
-; and cost the to that neighbour
-; (set-graph '((a (b 3)) (b (c 1) (d 5)) (c (d 2)) (d (b 2))))
+;;; weighted directed graph implementation
+;;; the graph is a list of nodes
+;;; a node is a list whose first element is the name of the node
+;;; and whose remaining elements are pairs of a neighbours name 
+;;; and cost the to that neighbour
+;;; (set-graph '((a (b 3)) (b (c 1) (d 5)) (c (d 2)) (d (b 2))))
 
 (setq graph nil)
 (defun set-graph (graph-as-list)
@@ -108,6 +128,14 @@
 ; tests
 
 (set-graph '((a (b 3)) (b (c 1) (d 5)) (c (d 2)) (d (b 2))))
-; (init-path-s graph)
-; (update-path- 'a 0)
-(dijkstra graph 'a)
+(init-path-costs graph)
+(update-path-cost 'a 0)
+(print path-costs)
+(init-predecessors graph)
+(print predecessors)
+(init-visited)
+(print visited)
+(init-candidates 'a)
+(print candidates)
+(path-cost 'a)
+; (dijkstra graph 'a)
